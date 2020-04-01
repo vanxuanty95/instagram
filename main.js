594c98961439208497d6b40c13a1469eaf36bd58
ty_xuan.v <ty_xuan.v@kbtg.tech>
2020-04-01 13:29:48 +0700

    quren

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ window.onload = function () {
 
     var feed = new Instafeed({
         links: 'https://www.instagram.com/p/B96Z85cJu-j/',
-        clientId: 'nhifake'
+        accessToken: 'IGQVJWaUJucHUybVVxUjNmblFPN2lKa0o4TFBUb3FoUTlCVjRBdnJlVkxhUW1GZADJoa2FZAZA2wxWmgyZAWR1NFRmMGlycXdJVlJNQ0xwdnhlTWc1OUlnVm5LcTFtQU1fc1AybXdYZADhoUGo0c2ZAsOUIzMAZDZD'
     });
     feed.run();
 
