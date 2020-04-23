29616951323f9ee127d65398f2f14298321967a2
ty_xuan.v <ty_xuan.v@kbtg.tech>
2020-04-23 18:12:16 +0700

    tesst

--- main.js
+++ main.js
@@ -117,7 +117,14 @@ $(document).ready(function() {
                 let urlFiltered = createImageWithFilter(document.getElementById('background_blur_img'))
                 $('#background_blur').attr("style", "background-image: url(" + urlFiltered + ")");
 
-                html2canvas(document.querySelector("#capture"), { scale: 2, allowTaint: true, useCORS: true }).then(function(canvas) {
+                html2canvas(document.querySelector("#capture"), {
+                    scale: 2,
+                    allowTaint: true,
+                    useCORS: true,
+                    removeContainer: true,
+                    backgroundColor: null,
+                    imageTimeout: 15000
+                }).then(function(canvas) {
                     canvas.id = "imageGenerated";
                     document.body.appendChild(canvas)
                     $("#downloadBtn").attr("disabled", false);
