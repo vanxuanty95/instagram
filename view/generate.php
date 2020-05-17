9bda4e210a5cb1e85c34d20b1a7efa5df2332d0f
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-17 11:23:45 +0700

    fix

--- view/generate.php
+++ view/generate.php
@@ -1,9 +1,5 @@
 <head>
     <title>Demo</title>
-    <link rel="stylesheet" type="text/css" href="assets/style.css">
-    <script async="" src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="assets/profile.js"></script>
 </head>
 
 <body>
