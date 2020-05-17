da112eae95c513e4bd6f2049dc1ac1eb9cb1d376
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-17 15:45:20 +0700

    fix

--- controller/generate.php
+++ controller/generate.php
@@ -24,16 +24,16 @@ class GenerateController extends BaseController
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" . $this->createTargetURL($username, $description) . "&key=" . $this->keyapi);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $googlePagespeedData = curl_exec($ch);
         curl_close($ch);
         $googlePagespeedData = json_decode($googlePagespeedData, true);
 
-        $data = array('error' => true);
         if ($googlePagespeedData["error"] == null) {
             $screenshot = $googlePagespeedData['lighthouseResult']['audits']['final-screenshot']['details']['data'];
-            $data = array('screenshot' => $screenshot, 'error' => false);
+            $data = array($screenshot);
+        } else {
+            $data = array('error' => $googlePagespeedData["error"]);
         }
         header('Content-Type: application/json');
         echo json_encode($data);
