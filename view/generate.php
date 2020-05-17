3b0c77179c1f7718ca6e78fb30b34a8656b0dd5b
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-17 11:18:39 +0700

    fix

--- view/generate.php
+++ view/generate.php
@@ -7,5 +7,5 @@
 </head>
 
 <body>
-    <img src="data:image/jpeg;base64,<?php echo $profile->username; ?>" />"
+    <img src="data:image/jpeg;base64,<?php echo $screenshot->screenshot; ?>" />"
 </body>
