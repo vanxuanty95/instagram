ae7484e3bff2ab6b3b91c0a5865ed357041b3a48
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-17 12:31:21 +0700

    chaneg method

--- controller/generate.php
+++ controller/generate.php
@@ -20,8 +20,13 @@ class GenerateController extends BaseController
 
     function getImageFromGoogleApi($username, $description)
     {
-
-        $googlePagespeedData = file_get_contents("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" . $this->createTargetURL($username, $description));
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch,CURLOPT_URL,"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" . $this->createTargetURL($username, $description));
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
+        $googlePagespeedData = curl_exec($ch);
+        curl_close($ch);
 
         var_dump($googlePagespeedData);
 
