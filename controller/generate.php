7eb2e2ea1ada3c2573d021817810134ecd577601
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-17 11:11:38 +0700

    Done

--- controller/generate.php
+++ controller/generate.php
@@ -4,6 +4,8 @@ require_once('service/generate.php');
 
 class GenerateController
 {
+    protected $generationApi = "http://instagram.nakamadressup.com/index.php?controller=profile&action=profile&username=__username&description=__description";
+
     function __construct()
     {
         $this->file = 'profile';
@@ -12,6 +14,28 @@ class GenerateController
     public function generate()
     {
         $profile = ProfileModel::set($_GET['username'], $_GET['description']);
-        return getImageFromGoogleApi($profile->username, $profile->description);
+        return $this->getImageFromGoogleApi($profile->username, $profile->description);
+    }
+
+    function getImageFromGoogleApi($username, $description)
+    {
+
+        $googlePagespeedData = file_get_contents("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" . $this->createTargetURL($username, $description) . "&screenshot=true");
+
+        //decode json data
+        $googlePagespeedData = json_decode($googlePagespeedData, true);
+
+        //screenshot data
+        $screenshot = $googlePagespeedData['screenshot']['data'];
+        $screenshot = str_replace(array('_', '-'), array('/', '+'), $screenshot);
+
+        //display screenshot image
+        return "<img src=\"data:image/jpeg;base64," . $screenshot . "\" />";
+    }
+
+    function createTargetURL($username, $description)
+    {
+        $target = str_replace(["__username", "__description"], [$username, $description], $this->generationApi);
+        return $target;
     }
 }
