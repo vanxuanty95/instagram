93800db38e3dcef5a8786be268d4c3d176d50c39
Ty Van <vanxuanty@TyVan-Hackintosh.local>
2020-05-19 15:11:02 +0700

    fix bug

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     });
 
     function resetVariable() {
-        $("#imageGenerated").remove;
+        $("#imageGenerated").remove();
     }
 
     function getInfomation(username, description) {
